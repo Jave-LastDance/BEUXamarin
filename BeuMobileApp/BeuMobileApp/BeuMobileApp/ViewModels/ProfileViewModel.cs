91ae682d7a3fd49f52d21f67f78e6f68f7eeb09b
JuanDavidAR <aldana.juand@javeriana.edu.co>
2023-11-12 16:57:39 -0500

    flujo boton preferencias

--- BeuMobileApp/BeuMobileApp/BeuMobileApp/ViewModels/ProfileViewModel.cs
+++ BeuMobileApp/BeuMobileApp/BeuMobileApp/ViewModels/ProfileViewModel.cs
@@ -9,11 +9,12 @@ namespace BeuMobileApp.ViewModels
     public class ProfileViewModel
     {
         public Command LoginCommand { get; }
+        public Command CJFDCommand { get; }
 
         public ProfileViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
-
+            CJFDCommand = new Command(OnCJFD);
         }
 
         private async void OnLoginClicked(object obj)
@@ -21,7 +22,11 @@ namespace BeuMobileApp.ViewModels
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(EventView)}");
         }
+        private async void OnCJFD(object obj)
+        {
 
+            await Shell.Current.GoToAsync($"{nameof(CJFDView)}");
+        }
         public UserResponse CurrentUser => App.CurrentUser;
     }
 }
